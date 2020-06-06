bcbe3336700ca983269be9928e5c57172d489ec5
elvishan <elvishan2008@gmail.com>
2020-06-06 17:04:19 +0900

    Solve multiple problems

---
+++ beack/src/p11654/Main.java
@@ -0,0 +1,13 @@
+package p11654;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		int c = System.in.read();
+		System.out.println(c);
+	}
+
+}
